2028477e90d265c1db033ac8061e57a73a6da15f
Cahaya Gilang Gustina <cahayagilanggustina01082000@gmail.com>
2024-04-04 20:51:12 +0700

    Update main-env.go

--- 1.0.1-env/main-env.go
+++ 1.0.1-env/main-env.go
@@ -1,28 +1,28 @@
 package main
 
-import {
+import (
 	"fmt"
 	"net/http"
 	"os"
-}
+)
 
 func helloWorld(w http.ResponseWriter, r *http.Request) {
 	message := os.Getenv("HELLO_MESSAGE")
 	if message == "" {
-		message = "Hello, World! (not set env)"
+		message = "Hello, World! (env variable not set)"
 	}
 	fmt.Fprintf(w, message)
 }
 
-func main()  {
+func main() {
 	http.HandleFunc("/", helloWorld)
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = "8080"
 	}
-	fmt.Println("Server running at http://localhost: %s\n" + port)
-	err := http.ListenAndServe(":"+port, nil)
+	fmt.Printf("Server running at http://localhost:%s\n", port) // Fixed printf format
+	err := http.ListenAndServe(":"+port, nil) // Added colon before port
 	if err != nil {
-		fmt.Printf("Failed to start server: %v\n",err)
+		fmt.Printf("Failed to start server: %v\n", err)
 	}
 }
